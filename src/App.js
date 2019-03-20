0c9c63eaaae08af32455e8386d82505fea740974
Stan-Sasquatch <stephenppercival@gmail.com>
2019-03-20 22:39:49 +0000

    added operation buttons

--- src/App.js
+++ src/App.js
@@ -7,38 +7,97 @@ class App extends Component {
   constructor(props) {
     super(props);
     
-this.state={output:0}
+this.state={output:0,
+  operation: ''
+}
 
-this.add=this.add.bind(this);
   }
 
   add = (number) => this.setState({output:this.state.output+number})
+  subtract = (number) => this.setState({output:this.state.output-number})
+  multiply = (number) => this.setState({output:this.state.output*number})
+  divide = (number) => this.setState({output:this.state.output/number})
+  setOperation = (symbol) => this.setState({operation:symbol})
+  
  ;
+changeOperation = (symbol) =>{
 
- addOnClick = (d)=> this.add(d);
+  
+  switch(symbol){
+ case '+':
+ this.setOperation('+');
+ break;
+case '-':
+this.setOperation('-');
+ break;
+ case '*':
+ this.setOperation('*')
+ break;
+case '/':
+this.setOperation('/')
+ break;
+default:
+ 
+  }
+}
+
+
+
+
+ operate = (number) => {
+   switch(this.state.operation){
+  case '+':
+  this.add(number);
+  break;
+case '-':
+  this.subtract(number);
+  break;
+  case '*':
+  this.multiply(number)
+  break;
+case '/':
+ this.divide(number)
+  break;
+default:
+  alert("Select an operator")
+   }
+}
+ 
 
   render() {
     return (
       <div className="App">
-<CalcButton value = {1} onClick = {this.add}
+<CalcButton value = {1} onClick = {this.operate}
 />
-<CalcButton value = {2} onClick = {this.add}
+<CalcButton value = {2} onClick = {this.operate}
 />
-<CalcButton value = {3} onClick = {this.add}
+<CalcButton value = {3} onClick = {this.operate}
 />
-<CalcButton value = {4} onClick = {this.add}
+<CalcButton value = {4} onClick = {this.operate}
 />
-<CalcButton value = {5} onClick = {this.add}
+<CalcButton value = {5} onClick = {this.operate}
 />
-<CalcButton value = {6} onClick = {this.add}
+<CalcButton value = {6} onClick = {this.operate}
 />
-<CalcButton value = {7} onClick = {this.add}
+<CalcButton value = {7} onClick = {this.operate}
 />
-<CalcButton value = {8} onClick = {this.add}
+<CalcButton value = {8} onClick = {this.operate}
 />
-<CalcButton value = {9} onClick = {this.add}
+<CalcButton value = {9} onClick = {this.operate}
 />
-<CalcButton value = {0} onClick = {this.add}
+<CalcButton value = {0} onClick = {this.operate}
+/>
+
+<CalcButton value = {'+'} onClick = {this.changeOperation}
+/>
+
+<CalcButton value = {'-'} onClick = {this.changeOperation}
+/>
+
+<CalcButton value = {'*'} onClick = {this.changeOperation}
+/>
+
+<CalcButton value = {'/'} onClick = {this.changeOperation}
 />
 
 <h1>{this.state.output}</h1>
