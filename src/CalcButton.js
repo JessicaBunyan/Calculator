f6e7def1cfad624170d0820888173b7dbde7cb51
Jessica Bunyan <tristan@bunyan.com>
2019-03-20 23:17:59 +0000

    new. stripped down button

--- src/CalcButton.js
+++ src/CalcButton.js
@@ -1,20 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 
- const CalcButton = (props) => {
-    
-    return (
-      <form>
-        <label>
-          <input
-            type="button"
-            value={props.value}
-            onClick={() => props.onClick(props.value)}
-          />
-        </label>
-      </form>
-    );
-  }
+const CalcButton = props => {
+  return (
+    <div
+      class="button"
+      value={props.value}
+      onClick={() => props.onClick(props.value)}
+    >
+      {props.value}
+    </div>
+  );
+};
 
-
- export default CalcButton;
+export default CalcButton;
